a1438f73df510e2a696711100c1d137ffb8d89a5
bo23519 <bo23519@gmail.com>
2025-12-22 14:25:17 -0500

    update convex schema

--- src/components/carousel/horizontalScrollCarousel/HorizontalScrollCarousel.tsx
+++ src/components/carousel/horizontalScrollCarousel/HorizontalScrollCarousel.tsx
@@ -60,20 +60,24 @@ export const HorizontalScrollCarousel = () => {
 };
 
 const Card = ({ project }: { project: CardType }) => {
-    // console.log(project.storageId);
     return (
         <div
             key={project._id}
             className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200"
         >
             <div className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110">
-                <div className="h-full w-full [&>img]:h-full [&>img]:w-full [&>img]:object-cover">
+                <div className="h-full w-full [&>img]:h-full [&>img]:w-full [&>img]:object-cover [&>img]:blur-sm">
                     <img src={project.storageId} alt={project.title} />
                 </div>
             </div>
             <div className="absolute inset-0 z-10 grid place-content-center">
-                <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
+                <p className="bg-gradient-to-br from-black/50 to-black/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
                     {project.title}
+                </p>
+            </div>
+            <div className="absolute inset-0 z-10 grid place-content-center">
+                <p className="bg-gradient-to-br from-black/50 to-black/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
+                    {project.tags.join(", ")}
                 </p>
             </div>
         </div>
@@ -82,12 +86,6 @@ const Card = ({ project }: { project: CardType }) => {
 
 export default HorizontalScrollCarousel;
 
-// type CardType = {
-//     url: string;
-//     title: string;
-//     id: number;
-// };
-
 type CardType = {
     _id: string;
     storageId: string;
@@ -95,41 +93,3 @@ type CardType = {
     description: string;
     tags: string[];
 };
-
-// const cards: CardType[] = [
-//     {
-//         url: "/imgs/abstract/1.jpg",
-//         title: "Title 1",
-//         id: 1,
-//     },
-//     {
-//         url: "/imgs/abstract/2.jpg",
-//         title: "Title 2",
-//         id: 2,
-//     },
-//     {
-//         url: "/imgs/abstract/3.jpg",
-//         title: "Title 3",
-//         id: 3,
-//     },
-//     {
-//         url: "/imgs/abstract/4.jpg",
-//         title: "Title 4",
-//         id: 4,
-//     },
-//     {
-//         url: "/imgs/abstract/5.jpg",
-//         title: "Title 5",
-//         id: 5,
-//     },
-//     {
-//         url: "/imgs/abstract/6.jpg",
-//         title: "Title 6",
-//         id: 6,
-//     },
-//     {
-//         url: "/imgs/abstract/7.jpg",
-//         title: "Title 7",
-//         id: 7,
-//     },
-// ];
