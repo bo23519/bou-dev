cf355f3d8f28b3ce14033a184a1660196edc2fa7
bo23519 <bo23519@gmail.com>
2025-12-24 10:29:43 -0500

    simple like button

---
+++ src/components/likeButton/LikeButton.tsx
@@ -0,0 +1,47 @@
+import { DrawOutlineButton } from "@/components/ui/button";
+import { ThumbsUpIcon } from "lucide-react";
+import { getLikes } from "../../../convex/stats";
+import { useQuery, useMutation } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export const LikeButton = () => {
+    const likes = useQuery(api.stats.getLikes);
+    const addLike = useMutation(api.stats.addLike);
+    const links = useQuery(api.links.getLinks);
+    const [showLiked, setShowLiked] = useState(false);
+
+    const HandleLike = () => {
+        addLike();
+        setShowLiked(true);
+        setTimeout(() => {
+            setShowLiked(false);
+        }, 1000);
+    }
+    
+
+    return (
+        <div>
+        <DrawOutlineButton onClick={() => HandleLike()}>
+            <div className="flex items-center gap-2">
+                <ThumbsUpIcon className="w-4 h-4 text-[#6366F1]"/>
+                <span className="text-[#6366F1]">{likes}</span>
+            </div>
+        </DrawOutlineButton>
+            <AnimatePresence>
+            {showLiked && 
+                <motion.div
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -10 }}
+                    transition={{ duration: 0.2 }}
+                    className="absolute -top-0 -translate-x-1/10 px-4 py-1 text-white text-sm font-medium rounded-md whitespace-nowrap"
+                >
+                    <ThumbsUpIcon className="w-4 h-4 text-[#6366F1]"/>
+                </motion.div>
+                }
+            </AnimatePresence>
+        </div>
+    );
+};
